9f6947adc0b14f280241e3d8d3653deb6ca726a9
braincy <lca1826@163.com>
2019-03-10 18:45:03 +0800

    初步定义后台页面

--- application/modules/Admin/controllers/Home.php
+++ application/modules/Admin/controllers/Home.php
@@ -5,4 +5,12 @@ class HomeController extends Base_Controller_Abstractlive {
     public function indexAction() {
         $this->getView()->display(APPLICATION_VIEW_PATH . 'Admin/Home/index.html');
     }
+
+    public function consoleAction() {
+        $this->getView()->display(APPLICATION_VIEW_PATH . 'Admin/Home/console.html');
+    }
+
+    public function pushAction() {
+        $this->getView()->display(APPLICATION_VIEW_PATH . 'Admin/Home/push.html');
+    }
 }
